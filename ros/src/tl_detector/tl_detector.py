68f3f07b525ab591f76002eff36b3047dce50ae9
Bibigul Shektybayeva <bibigul.shektybayeva@bmw.de>
2019-03-04 02:51:54 +0100

    initialize subscribers later

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -25,6 +25,24 @@ class TLDetector(object):
         self.camera_image = None
         self.lights = []
 
+
+        config_string = rospy.get_param("/traffic_light_config")
+        self.config = yaml.load(config_string)
+
+        self.bridge = CvBridge()
+
+
+        self.state = TrafficLight.UNKNOWN
+        self.last_state = TrafficLight.UNKNOWN
+        self.last_wp = -1
+        self.state_count = 0
+        self.class_count = 0
+        self.process_count = 0
+
+        self.light_classifier = TLClassifier(rospy.get_param('~model_file'))
+        self.listener = tf.TransformListener()
+
+
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
@@ -38,21 +56,9 @@ class TLDetector(object):
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
-        config_string = rospy.get_param("/traffic_light_config")
-        self.config = yaml.load(config_string)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
-        self.bridge = CvBridge()
-        self.light_classifier = TLClassifier(rospy.get_param('~model_file'))
-        self.listener = tf.TransformListener()
-
-        self.state = TrafficLight.UNKNOWN
-        self.last_state = TrafficLight.UNKNOWN
-        self.last_wp = -1
-        self.state_count = 0
-        self.class_count = 0
-        self.process_count = 0
 
         rospy.spin()
 
